d15cf8e18e8a6dfb53dd3da95b2474636ee3dfde
blackman1380 <mousavi.asadollah@gmail.com>
2016-09-15 19:45:23 +0430

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -1,6 +1,7 @@
 HTTP = require('socket.http')
 HTTPS = require('ssl.https')
 URL = require('socket.url')
+JSON = require('dkjson')
 redis = require('redis')
 clr = require 'term.colors'
 db = Redis.connect('127.0.0.1', 6379)
