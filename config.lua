ce4dec27516537c540297a4c73abb78b914d4641
blackman1380 <mousavi.asadollah@gmail.com>
2016-09-15 19:30:26 +0430

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,5 +1,5 @@
 return {
-	bot_api_key = '199042636:AAG7BrFZTwgO87myNvd3q0zEdGHV3A1pJ9E'
+	bot_api_key = '199042636:AAG7BrFZTwgO87myNvd3q0zEdGHV3A1pJ9E,'
 	version = '4.1.2',
 	cmd = '^[/!#]',
 	db = 2, --default redis db: 0
